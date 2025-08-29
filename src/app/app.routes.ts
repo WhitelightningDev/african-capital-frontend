85e8fa6749c96e035714f6acb32980957e9b04e2
Daniel Mommsen <danielmommsen@hotmail.com>
2025-08-29 22:57:00 +0200

    added a login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 export const routes: Routes = [
-  
+  {
+    path:"", component: LoginPageComponent
+  }
 ];
