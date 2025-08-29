34c2d0798c7dec9ce4d8a647c94f45ff5c6584d3
Daniel Mommsen <danielmommsen@hotmail.com>
2025-08-29 23:40:20 +0200

    `Added Register page component and updated routes to include it`

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
 
 export const routes: Routes = [
   {
     path:"", component: LoginPageComponent
+  },
+  {
+    path:"Register", component: RegisterPageComponent
   }
 ];
--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.scss'],
+  imports: [IonContent],
 })
 export class RegisterPageComponent  implements OnInit {
 
