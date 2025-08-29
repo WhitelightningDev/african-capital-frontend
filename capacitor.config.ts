658ce1caaf64368be3de991ae6fec48f98c75436
Daniel Mommsen <danielmommsen@hotmail.com>
2025-08-29 22:20:38 +0200

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'african-capital-frontend',
+  webDir: 'www'
+};
+
+export default config;
